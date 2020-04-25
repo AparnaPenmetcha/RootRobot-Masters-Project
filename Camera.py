d02494bb4019b3b2cb10f238cb921f297fef7fdc
Aparna Penmetcha <aparna.penmetcha@tufts.edu>
2020-04-25 16:48:25 -0400

    added handshake

--- Camera.py
+++ Camera.py
@@ -6,43 +6,57 @@ import cv2
 import rospy
 from std_msgs.msg import String
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-#rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
 
 
+class Camera:
 
-def root_callback(msg):
+    def __init__(self):
+        rospy.init_node('RootTest', anonymous='True')
 
-    # print('checkpoint5')
+        # Prepare publisher on the 'sendRoot' topic
+        self.pub = rospy.Publisher('toRoot', String, queue_size=10)
+        rospy.Subscriber('fromRoot', String, self.root_callback)
+        self.received = False
+        self.camera = PiCamera()
 
-    # camera.start_preview()
-    print(msg.data)
-    # time.sleep(5)
-    # camera.capture('/home/pi/Desktop/testExample.jpg')
-    # camera.stop_preview()
 
-def main():
-    """Node setup and main ROS loop"""
-    print('checkpoint1')
-    rospy.init_node('RootTest', anonymous='True')
-    print('checkpoint2')
+    def sendRequest(self, message):
 
-    #Prepare publisher on the 'sendRoot' topic
-    pub = rospy.Publisher('toRoot', String, queue_size=10)
-    rospy.Subscriber('fromRoot', String, root_callback)   # Use the 'sendRoot' topic
-    print('checkpoint3')
+        # rospy.loginfo('Sending to Camera: %s', messageString.data)
+        msg = String()
+        rate = rospy.Rate(1)  # update rate in Hz
+        if message == 'received':
+            n = 0
+            while not rospy.is_shutdown():
+                msg.data = message
+                rospy.loginfo('%s(%.2f) %s' % (rospy.get_name(), rospy.get_time(), message))
+                self.pub.publish(msg)
+                n = n+1
+                if n>100:
+                    return
+        else:
+            while not rospy.is_shutdown():
+                msg.data = message
+                rospy.loginfo('%s(%.2f) %s' % (rospy.get_name(), rospy.get_time(), message))
+                self.pub.publish(msg)
+                if self.received:
+                    self.received = False
+                    return
 
-    rospy.spin()
+        print('checkpoint7')
 
-    print('checkpoint4')
+    def root_callback(self, msg):
 
-    # msg = String()
-    # rate = rospy.Rate(1)  #update rate in Hz
-    # command=['red','green','blue']
-    
+            # print('checkpoint5')
+            self.sendRequest('received')
+            # camera.start_preview()
+            print(msg.data)
+            # time.sleep(5)
+            # camera.capture('/home/pi/Desktop/testExample.jpg')
+            # camera.stop_preview()
+
+
 
 if __name__ == '__main__':
-    main()
+    camera = Camera()
+    rospy.spin()
--- RootController.py
+++ RootController.py
@@ -13,44 +13,48 @@ class RootController:
         rospy.init_node('rootPi', anonymous=True)
         rospy.Subscriber('toRoot', String, self.root_callback)
         self.pub = rospy.Publisher('fromRoot', String, queue_size=10)
-        print('checkpoint2')
+        self.received = False
 
-        print('checkpoint3')
-    
     def sendRequest(self, message):
-        print('checkpoint5')
-        messageString = String()
-        messageString.data = message
-        print('checkpoint6')
 
         # rospy.loginfo('Sending to Camera: %s', messageString.data)
         msg = String()
         rate = rospy.Rate(1)  # update rate in Hz
-        command = ['red', 'green', 'blue']
-        n = 0
-        while not rospy.is_shutdown():
-            for cmd in command:
-                msg.data = str(n) + ':' +cmd
-                rospy.loginfo('%s(%.2f) %s' % (rospy.get_name(), rospy.get_time(), msg.data))
+        if message == 'received':
+            n = 0
+            while not rospy.is_shutdown():
+                msg.data = message
+                rospy.loginfo('%s(%.2f) %s' % (rospy.get_name(), rospy.get_time(), message))
                 self.pub.publish(msg)
                 n = n + 1
-                if n > 50:
+                if n > 100:
                     return
-        # self.pub.publish(messageString)
-        print('checkpoint7')
+        else:
+            while not rospy.is_shutdown():
+                msg.data = message
+                rospy.loginfo('%s(%.2f) %s' % (rospy.get_name(), rospy.get_time(), message))
+                self.pub.publish(msg)
+                if self.received:
+                    self.received = False
+                    return
 
-
-    def root_callback(self,msg):
-            rospy.loginfo('I heard %s.', msg.data)
-
+    def root_callback(self, msg):
+        if msg.data == 'received':
+            self.received = True
+        else:
+            # print('checkpoint5')
+            self.sendRequest('received')
+            # camera.start_preview()
+            print(msg.data)
+            # time.sleep(5)
+            # camera.capture('/home/pi/Desktop/testExample.jpg')
+            # camera.stop_preview()
 
 if __name__ == '__main__':
 
-    print('checkpoint1')
     rootController = RootController()
-    print('checkpoint4')
     rootController.sendRequest('getOs')
-    print('checkpoint8')
+    
 
     rospy.spin()
         
