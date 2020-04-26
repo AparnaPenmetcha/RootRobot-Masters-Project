c7a859babd6476cafa8e2c47aabb89d0d91aef8b
Aparna Penmetcha <aparna.penmetcha@tufts.edu>
2020-04-26 16:25:28 -0400

    slowing publisher

--- publisher.py
+++ publisher.py
@@ -6,7 +6,7 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(0.1) # 10hz
     n = 0
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % str(n)
