7a61954f4b6f89f72f82b63c8c8ab3b81c6da245
Aparna Penmetcha <aparna.penmetcha@tufts.edu>
2020-04-26 16:22:57 -0400

    added n

--- publisher.py
+++ publisher.py
@@ -7,12 +7,13 @@ def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    n = 0
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = "hello world %s" % str(n)
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
-
+        n = n+1
 if __name__ == '__main__':
     try:
         talker()
